53524a82cfa0fe0d3c632c177555e2e2ffb479e4
Gero3 <jeroen-876@hotmail.com>
2012-02-06 03:05:17 +0100

    make sure that nodejs0.7 also works.

--- support/paths.js
+++ support/paths.js
@@ -5,3 +5,16 @@
 
 require("./requireJS-node");
 
+var defaultResolver = module.constructor._resolveLookupPaths;
+
+var Resolves = {"sys":"util"};
+
+module.constructor._resolveLookupPaths = function(request, parent) {
+    
+    if (Resolves[request]){
+        return defaultResolver(Resolves[request],parent);
+    } else {
+        return defaultResolver(request,parent);
+    };
+    
+};
